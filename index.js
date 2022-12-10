27389a5c59948384307c33c082b8162f424335c3
masaaki_komatsubara <78458742+mikanfirst@users.noreply.github.com>
2022-12-10 18:59:44 +0900

    [delete]コメントアウトの削除

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-// const request = new XMLHttpRequest();
-// request.open("GET", "https://www.googleapis.com/books/v1/volumes?q=%E5%A4%8F%E7%9B%AE%E6%BC%B1%E7%9F%B3");
-// request.send();
-
-// const getData = function() {
-//     fetch("https://www.googleapis.com/books/v1/volumes?q=%E5%A4%8F%E7%9B%AE%E6%BC%B1%E7%9F%B3")
-//     .then((res) => {
-//         console.log(res)
-//     });
-// };
-
-// getData();
-
-// const getData = function () {
-//     fetch("https://www.googleapis.com/books/v1/volumes?q=%E5%A4%8F%E7%9B%AE%E6%BC%B1%E7%9F%B3")
-//     .tnen((res) => res.json())
-//     .then((apiData) => console.log(apiData));
-// };
-// getData();
-
-// let url = 'https://www.googleapis.com/books/v1/volumes?q=%E5%A4%8F%E7%9B%AE%E6%BC%B1%E7%9F%B3';
-
-// fetch(url)
-//     .them(Response => Response.json())
-//     .then(data => {
-//         console.log(data);
-//     });
+// ＜＜課題＞＞
+// fetchでurlからapiにアクセス
+// その情報を取得し、コンソールに表示
+// その際に、promiseやasyncを使うこと
 
 async function fetchData(url) {
     try {
